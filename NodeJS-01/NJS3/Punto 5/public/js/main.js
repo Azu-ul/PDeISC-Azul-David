245c6e95ce72f78750bafab5858a1747e0219734
Azul Sofía David <azulsofiadavid@gmail.com>
2025-04-30 14:14:21 -0300

    hice el punto 5

---
+++ NodeJS-01/NJS3/Punto 5/public/js/main.js
@@ -0,0 +1,15 @@
+document.getElementById('addDiv').addEventListener('click', () => {
+    const nuevoDiv = '<div class="nuevoElemento">Este es un nuevo Div</div>';
+    document.getElementById('contenido').innerHTML += nuevoDiv;
+  });
+  
+  document.getElementById('addParagraph').addEventListener('click', () => {
+    const nuevoP = '<p class="nuevoElemento">Este es un nuevo P (párrafo)</p>';
+    document.getElementById('contenido').innerHTML += nuevoP;
+  });
+  
+  document.getElementById('addLink').addEventListener('click', () => {
+    const nuevoEnlace = '<a href="https://www.google.com" target="_blank" class="nuevoElemento">Ir a Google</a>';
+    document.getElementById('contenido').innerHTML += nuevoEnlace;
+  });
+  
